e500e0bdc42e5d2e4b3151186d111b7a4fe99d92
Zak <burkard.exe@gmail.com>
2025-07-12 19:07:49 -0600

    commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react"
+import Search from "./components/Search.jsx"
+
+const API_BASE_URL = "https://api.themoviedb.org/3"
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer ${API_KEY}'
+  }
+}
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint =
+      '${API_BASE_URL}/discover/movie?sort_by=popularity.desc';
+      const response = await fetch(endpoint, API_OPTIONS);
+    } catch (error) {
+      console.error('Error fetching movies: ${error}');
+      setErrorMessage('Error fetching movies. Please try again later.');
+    }
+  }
+
+  useEffect(()=> {
+
+  },[])
+  return (
+    <main>
+      <div className="pattern"/>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section>
+          <h2>All Movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
+        
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = ({ searchTerm, setSearchTerm}) => {
+  return (
+    <div className="search">
+      <div>
+      <img src="search.svg" alt="search" />
+      <input type="text" placeholder="Search through thousand of movies" value={searchTerm} onChange={(event)=> setSearchTerm(event.target.value)} />
+      </div>
+    </div>
+  )
+}
+
+export default Search
